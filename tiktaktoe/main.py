3f554183288b0c4329530787d22a7b093e042929
idk-tutel <franusz.bananusz@gmail.com>
2025-05-14 09:48:58 +0200

    lol

--- tiktaktoe/main.py
+++ tiktaktoe/main.py
@@ -13,6 +13,12 @@ def draw():
     print(" " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " ")
     print("---|---|---")
     print(" " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " ")
+def logic():
+    for i in range(9):
+        turn="o"
+        draw()
+        while pc != True:
+            pc = move(input("row"), input("column"))
 
 
 
@@ -28,3 +34,5 @@ def draw():
 #        |   |
 #      --|---|--
 #        |   |
+
+# MOZE poTEM SZACHY?????
