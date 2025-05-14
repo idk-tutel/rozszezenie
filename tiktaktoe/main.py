01e3ff9e0c210bcbc40a3fa763664dd004b8eb1b
idk-tutel <franusz.bananusz@gmail.com>
2025-05-14 09:32:52 +0200

    tictactoe nie dzala

--- tiktaktoe/main.py
+++ tiktaktoe/main.py
@@ -1,1 +1,30 @@
-print("cos i zmiana")
+board = [[" ", " ", " "],
+         [" ", " ", " "],
+         [" ", " ", " "]]
+def move(x, y, kto):
+    if board[y][x] == " ":
+        board[y][x] = kto
+        return True
+    else:
+        return False
+def draw():
+    print(" " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " ")
+    print("---|---|---")
+    print(" " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " ")
+    print("---|---|---")
+    print(" " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " ")
+
+
+
+
+
+
+
+
+
+
+#        |   |
+#      --|---|--
+#        |   |
+#      --|---|--
+#        |   |
